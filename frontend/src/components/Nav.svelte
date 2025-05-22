a573983e360f33f0aefbc4093a42924f363780f0
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-05-23 01:41:04 +0530

    fe: nav-bar -> add Roboto font

--- frontend/src/components/Nav.svelte
+++ frontend/src/components/Nav.svelte
@@ -1,4 +1,7 @@
 <style>
+    /* fonts */
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+
     .card{
         display: flex;
         justify-content: space-between;
@@ -20,13 +23,20 @@
         padding: 0.5rem;
         
         border-radius: 4px;
-        color: #1A9DFA;
+        color: #027bd1;
         text-decoration: none;
+
+        /* font */
+        font-family: "Roboto", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 350;
+        font-style: normal;
+        font-variation-settings:"wdth" 100;
     }
    
    .nav-links > a:hover{
         color: aliceblue;
-        background-color: #1A9DFA;
+        background-color: #027bd1;
 
    }
 </style>
@@ -41,7 +51,7 @@
     <a href="/about">About</a>
     <a href="/docs">Docs</a>
     <a href="https://dev.to/axonscript">Blog</a>
-    <a href="">Community</a>
+    <a href="/community">Community</a>
     <a href="https://github.com/AxonScript">GitHub</a>
 </ul>
 </div>"
