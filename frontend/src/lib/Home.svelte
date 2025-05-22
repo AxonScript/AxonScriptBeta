e51cbbecd1c48d6100ff8ca59fe8533a29521490
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-05-23 00:31:16 +0530

    frontend: add nav component
    nav bar component

--- frontend/src/lib/Home.svelte
+++
@@ -1,1 +0,0 @@
-<h1>Axon Script</h1>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,13 +1,13 @@
 // router.js
-import Home from "./lib/Home.svelte";
+import Home from "./components/Home.svelte";
 
 // import { wrap } from "svelte-spa-router/wrap";
 
 const routes = {
   "/": Home,
-  /* 
-  "/about": About,
-  "*": Error404, // Catch-all route for 404 errors */
+
+  // "/about": About,
+  // "*": Error404, // Catch-all route for 404 errors
 };
 
 export default routes;
