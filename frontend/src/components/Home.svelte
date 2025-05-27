dbb3aeb36d99fe4d35f9dc19fdd40a1fe0864a80
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-05-27 21:41:14 +0530

    fe: add sass package

--- frontend/src/components/Home.svelte
+++ frontend/src/components/Home.svelte
@@ -1,4 +1,4 @@
-<script>
+<script>  
   import Nav from "./Nav.svelte";
 
 </script>
--- frontend/src/components/Nav.svelte
+++ frontend/src/components/Nav.svelte
@@ -1,65 +1,76 @@
-<style>
+
+
+<style lang="scss">
     /* fonts */
     @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+
+
+    :root{
+        --nav-bg: black;
+        --nav-links-color-on-hover: black;
+    }
+    [nav-theme="light"]{
+        --nav-bg: aliceblue;
+        --nav-links-color-on-hover: aliceblue;
+    }
+
+
 
     .card{
         display: flex;
         justify-content: space-between;
         padding: 1rem;
-        /* background-color:aliceblue;         */
-        background: #000;
-    }
-
-
-    .nav-img{
-        height: 3rem;
-    }
-
-    .nav-links{
-        margin-top: 1rem;
-    }
-    .nav-links > a{
-        font-size: 1rem;
-        margin-right: 2rem;
-        padding: 0.5rem;
+        background: var(--nav-bg);
         
-        border-radius: 4px;
-        color: #027bd1;
-        text-decoration: none;
+        
+        .nav-logo{
+            height: 3rem;
+        }
 
-        /* font */
-        font-family: "Varela Round", sans-serif;
-        font-weight: 1000;
-        font-style: normal;
+
+        .nav-links{
+            margin-top: 1rem;
+
+            
+            a{
+                font-size: 1rem;
+                margin-right: 2rem;
+                padding: 0.5rem;
+            
+                border-radius: 4px;
+                color: #027bd1;
+                text-decoration: none;
+
+                /* font */
+                font-family: "Varela Round", sans-serif;
+                font-weight: 1000;
+                font-style: normal;
+
+                &:hover{
+                    color: var(--nav-links-color-on-hover);
+                    
+                    
+                    background-color: #027bd1;
+                    font-weight: 900;
+
+               }
+            }
+        }
     }
-   
-   .nav-links > a:hover{
-        /* color: aliceblue; */
-        color: #000;
-        background-color: #027bd1;
-        font-weight: 900;
-
-   }
 
 
-
-   /* light theme  */
-   .light-theme {
-        background: aliceblue;
-   }
-   .light-theme > .nav-links > a:hover{
-        color: aliceblue;
-
-   }
+    
+    
 </style>
 
 
 
 <div class="card ">
     <a href="/">
-        <img class="nav-img" src="/axonscript-favicon.png" alt="AxonScript Logo">
+        <img class="nav-logo" src="/axonscript-favicon.png" alt="AxonScript Logo">
     </a>
 <ul class="nav-links">
+    
     <a href="/about">About</a>
     <a href="/docs">Docs</a>
     <a href="https://dev.to/axonscript">Blog</a>
