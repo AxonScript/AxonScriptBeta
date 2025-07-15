22d44bea2b856b6ce20346d4fedf329e32da7005
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-07-15 12:26:00 +0530

    fe: Complete restart in Angular

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: ` <h1>Hello AxonScript!</h1> `,
+  styles: `
+    h1{
+      text-align: center;
+      font-size: 4rem;
+    }
+  `,
+})
+export class App {}
