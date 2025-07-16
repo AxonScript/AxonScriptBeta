cabbd235e6d7f0eebc408727a5e6a646aad03557
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-07-16 15:16:49 +0530

    fe: nav bar complete

--- frontend/src/app/components/nav-bar/nav-bar.ts
+++ frontend/src/app/components/nav-bar/nav-bar.ts
@@ -7,4 +7,22 @@ import { RouterLink } from '@angular/router';
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss',
 })
-export class NavBar {}
+export class NavBar {
+  theme_toggle() {
+    const body = document.body;
+    const currentTheme: any = body.getAttribute('theme');
+    const theme_toggle_btn: any = document.getElementById('theme-toggle-btn');
+
+    if (currentTheme == 'dark') {
+      theme_toggle_btn.innerHTML = `
+        <img src="/dark-theme-icon.png" alt="dark-theme-icon" height="25rem" />
+    `;
+      body.setAttribute('theme', 'light');
+    } else {
+      theme_toggle_btn.innerHTML = `
+        <img src="/light-theme-icon.png" alt="light-theme-icon" height="25rem" />
+    `;
+      body.setAttribute('theme', 'dark');
+    }
+  }
+}
