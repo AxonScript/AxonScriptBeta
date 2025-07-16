f1994e458319718dc6c970f565854ea5f8ee7b3d
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-07-17 03:16:52 +0530

    fe: Add Hero section to Home

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavBar } from '../components/nav-bar/nav-bar';
-
+import { Hero } from '../components/hero/hero';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBar],
+  imports: [NavBar, Hero],
   template: ` <div class="container">
     <app-nav-bar />
+    <app-hero />
   </div>`,
   styles: `
     .container{
